c59cc0fc10138ca8ad2c091dc049ca62cf096916
luke <125353302+lukeduck321@users.noreply.github.com>
2025-11-05 19:27:44 +1030

    Add files via upload

---
+++ py_game/game.py
@@ -0,0 +1,52 @@
+from tkinter import *
+from random import randint
+
+def game_start(window):
+    mousex = 1
+    mousey = 1
+    x = 1
+    y = 1
+
+    ca = Canvas(window, width = 400, height = 600, background = "white")
+    ca.pack()
+
+    def spawn():
+        npcball = randint(0, 400)
+        ca.create_oval(npcball-5,npcball-5,npcball+5,npcball+5)
+
+    def click(e):
+
+        nonlocal x, y
+        
+        ballx = e.x
+        bally = e.y
+    
+        print(ballx,bally)
+        
+        ca.create_oval(ballx-5,bally-5,ballx+5,bally+5)
+
+
+        print("click")   
+
+    def callback(e):
+        global x, y
+        nonlocal mousex, mousey
+    
+        ca.create_line(mousex,mousey,200,600, fill ="white")
+
+        x = e.x
+        y = e.y
+
+        mousex = x
+        mousey = y
+
+        if mousey < 500:
+            mousey = 500
+        
+        ca.create_line(mousex,mousey,200,600)
+        print(x,y)
+
+    spawn()
+
+    ca.bind('<Motion>', callback)
+    ca.bind('<Button-1>', click)
---
+++ py_game/main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from game import *
+
+window = Tk()
+window.geometry("400x600")
+
+
+game_start(window = window)
+
+
+window.mainloop()
